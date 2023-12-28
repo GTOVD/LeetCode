cf40e7dc99554e79d39b8810ac8da5cbfd72b4fb
GTOVD <42597732+GTOVD@users.noreply.github.com>
2023-12-28 15:23:32 -0600

    Time: 112 ms (5.64%) | Memory: 42.1 MB (39.98%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    nums.splice(0,nums.length,...nums.filter(num=>num!==val))
+    return nums.length
+};
