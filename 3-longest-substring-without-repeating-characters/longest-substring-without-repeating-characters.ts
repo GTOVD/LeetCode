40f97cbf20ebd9497336edea81b73d093243490e
GTOVD <42597732+GTOVD@users.noreply.github.com>
2023-12-28 20:28:49 -0600

    Time: 2413 ms (5.03%) | Memory: 52.5 MB (8.18%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,20 +1,35 @@
 function lengthOfLongestSubstring(s: string): number {
-    let index = 0;
-    const strSet = new Set()
-    let allSets = []
+ const scanner: string[] = []
+  let longest = 0
 
-    if(s.length <= 1) return s.length;
+  for (const char of s) {
+    console.log(scanner)
+    const possibleIndex = scanner.indexOf(char)
+    console.log(char, possibleIndex)
+    if (possibleIndex !== -1) { scanner.splice(0, possibleIndex + 1) }
+    scanner.push(char)
+    longest = Math.max(longest, scanner.length)
+  }
 
-    while(index < s.length) {
-        if(strSet.has(s[index])) {
-            allSets.push(strSet.size)
-            if(strSet.size>=2) index-=strSet.size-1
-            strSet.clear()
-        } else {
-            strSet.add(s[index])
-            index++
-        }
-    }
+  return longest
 
-    return Math.max(...allSets, strSet.size)
+
+    // let index = 0;
+    // const strSet = new Set()
+    // let allSets = []
+
+    // if(s.length <= 1) return s.length;
+
+    // while(index < s.length) {
+    //     if(strSet.has(s[index])) {
+    //         allSets.push(strSet.size)
+    //         if(strSet.size>=2) index-=strSet.size-1
+    //         strSet.clear()
+    //     } else {
+    //         strSet.add(s[index])
+    //         index++
+    //     }
+    // }
+
+    // return Math.max(...allSets, strSet.size)
 };
