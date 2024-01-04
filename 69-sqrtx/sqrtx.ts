806d5588a68f861997c44be9127b2d11bc367b08
GTOVD <42597732+GTOVD@users.noreply.github.com>
2024-01-03 20:54:04 -0600

    Time: 79 ms (37.70%) | Memory: 44.7 MB (66.40%) - LeetSync

--- 69-sqrtx/sqrtx.ts
+++ 69-sqrtx/sqrtx.ts
@@ -1,3 +1,46 @@
 function mySqrt(x: number): number {
-    return Math.floor(x**0.5);
+    // return Math.floor(Math.sqrt(x));
+
+    // return Math.floor(x**0.5);
+
+    // return equationSqrt(x);
+
+    // return iteratingSqrt(x);
+
+    return binarySearchSqrt(x);
 };
+
+const equationSqrt = (x: number): number => {
+    if(x < 2) return x;
+
+    return x
+}
+
+const iteratingSqrt = (x: number): number => {
+    if(x < 2) return x;
+
+    x*x === x
+
+    return x
+}
+
+const binarySearchSqrt = (x: number): number => {
+    if(x < 2) return x;
+
+    let high = x;
+    let low = 1;
+
+    while(low<=high) {
+        let middle = Math.floor((high+low)/2)
+
+        if(middle*middle === x) return middle;
+
+        if(middle*middle < x) low=middle+1;
+
+        else { 
+            high=middle-1;
+        }
+    }
+
+    return high
+}
