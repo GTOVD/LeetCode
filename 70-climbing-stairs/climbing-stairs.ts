542def0e1bb94791e4e11195e3fe3ed425498a1e
GTOVD <42597732+GTOVD@users.noreply.github.com>
2024-01-03 22:23:19 -0600

    Time: 63 ms (19.68%) | Memory: 45.4 MB (6.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.ts
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -1,7 +1,23 @@
 function climbStairs(n: number, memo={}): number {
-    if(n<=3) return n;
-    if(!memo[n]) memo[n] = (climbStairs(n-1, memo) + climbStairs(n-2, memo))
-    return memo[n]
+    if(n<=2) return n;
+
+    const table = [];
+    table[1] = 1;
+    table[2] = 2;
+
+    for(let i = 3; i<=n; i++) {
+        table[i]= table[i-1] + table[i-2]
+    }
+
+    console.log(table)
+
+    return table[n]
+
+
+    // console.log(memo)
+    // if(n<=3) return n;
+    // if(!memo[n]) memo[n] = (climbStairs(n-1, memo) + climbStairs(n-2, memo))
+    // return memo[n]
 
     // let result = 0;
     // let one = 0
