5646949d4bf6652c8a6928fa261e68f08b5cdd4c
GTOVD <42597732+GTOVD@users.noreply.github.com>
2024-01-05 00:54:09 -0600

    Time: 119 ms (5.08%) | Memory: 52.1 MB (8.74%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
@@ -14,22 +14,22 @@
 
 function inorderTraversal(root: TreeNode | null): number[] {
     if(!root) return [];
-    return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)]
+    // return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)]
 
-    // let stack = []
-    // let result = []
-    // let head = root
+    let stack = []
+    let result = []
+    let head = root
 
-    // while(head || stack.length > 0) {
-    //     while ( head ) {
-    //         console.log({stack,head})
-    //         stack.push(head)
-    //         head = head.left
-    //     }
-    //         console.log({stack,head})
-    //     head = stack.pop()
-    //     result.push(head.val)
-    //     head = head.right
-    // }
-    // return result
+    while(head || stack.length > 0) {
+        while ( head ) {
+            console.log({stack,head})
+            stack.push(head)
+            head = head.left
+        }
+            console.log({stack,head})
+        head = stack.pop()
+        result.push(head.val)
+        head = head.right
+    }
+    return result
 };
