5a891668d8a235614f1288ecfe1eda8ab06ea6d2
GTOVD <42597732+GTOVD@users.noreply.github.com>
2024-01-08 20:15:37 -0600

    Time: 73 ms (12.95%) | Memory: 46.1 MB (5.11%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.ts
+++ 118-pascals-triangle/pascals-triangle.ts
@@ -1,10 +1,10 @@
 function generate(numRows: number): number[][] {
-    console.log(numRows)
     let results = []
 
     for(let i = 1; i<=numRows; i++) {
         results.push(new Array(i).fill(1))
     }
+    
     results = results.map((result, i)=> {
         console.log(result)
         if(result.length <= 2) return result
