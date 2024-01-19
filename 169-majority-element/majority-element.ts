6a05f9b2fdddefa754fc9c52ce6b7fdb19af6f60
GTOVD <42597732+GTOVD@users.noreply.github.com>
2024-01-18 18:50:29 -0600

    Time: 70 ms (28.57%) | Memory: 46.6 MB (23.32%) - LeetSync

--- 169-majority-element/majority-element.ts
+++ 169-majority-element/majority-element.ts
@@ -1,11 +1,4 @@
 function majorityElement(nums: number[]): number {
-    let answer = {}
-    nums.forEach((num)=>{
-        if(answer[num]) answer[num]++
-        else answer[num] = 1
-    })
-    for(let [key, value] of Object.entries(answer)) {
-        if(+value > (Math.floor(nums.length/2))) return +key
-    }
-    return null
+    nums.sort()
+    return nums[Math.floor(nums.length/2)]
 };
