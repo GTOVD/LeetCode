be703d85e4b762c37cd1f4facad50180551518db
Thomas Vickers <42597732+GTOVD@users.noreply.github.com>
2024-07-24 23:04:22 -0600

    Time: 426 ms (29.45%) | Memory: 50.6 MB (83.73%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.js
+++ 10-regular-expression-matching/regular-expression-matching.js
@@ -5,7 +5,6 @@
  */
 var isMatch = function(s, p) {
     p = p.replace(/\*{1,}/g, "{0,}")
-    console.log(p)
     let regex = new RegExp(`^${p}$`, "g");
     let ans = regex.test(s)
     return ans
