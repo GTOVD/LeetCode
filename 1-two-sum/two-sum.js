7654a9253a4b84cd8b756b18485aa6d6070749c2
Thomas Vickers <42597732+GTOVD@users.noreply.github.com>
2024-07-25 23:35:25 -0600

    Time: 38 ms (99.86%) | Memory: 52.8 MB (5.13%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,12 +3,8 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let ans = []
-    for(let i=0; i<nums.length; i++) {
-        for(let j=i+1; j<nums.length; j++) {
-            if(nums[i] + nums[j] === target) ans.push(...[i,j])
-        }
-    }
-    return ans
+var twoSum = function(nums, target, memo={}, num=0) {
+    if(nums[num] in memo) return [memo[nums[num]], num]
+    memo[target -nums[num]] = num
+    return twoSum(nums, target, memo, ++num)
 };
